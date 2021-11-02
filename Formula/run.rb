614cfd3608ec8779ae7302fd140acb53969ee882
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-11-02 19:38:18 +0100

    Add run formula

---
+++ Formula/run.rb
@@ -0,0 +1,15 @@
+class Run < Formula
+  desc "Utility to run scripts out of GitHub repositories"
+  homepage "https://github.com/RiddleMan/run"
+  url "https://github.com/RiddleMan/run/releases/download/v0.0.3/run-0.0.3.tar.gz"
+  sha256 "279410ee7631af0c927652fc3f709f705f0bcd2f8110530b1e522d44b3066555"
+  license "MIT"
+
+  def install
+    system "./install.sh"
+  end
+
+  test do
+    system "true"
+  end
+end
