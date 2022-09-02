78d5a4221264a6048f60d90db23e11c81542eee0
Artur Ptaszek <artur.ptaszek@gmail.com>
2022-09-02 10:57:06 +0200

    feat: Bump doken version

--- Formula/doken.rb
+++ Formula/doken.rb
@@ -1,8 +1,8 @@
 class Doken < Formula
   desc "Tool for getting tokens from OAuth 2/OpenID Connect providers"
   homepage "https://github.com/RiddleMan/doken"
-  url "https://github.com/RiddleMan/doken/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f862fd9d2b34f87437dbbcafa8f220dfeb2c9591d1cab729500f431de40217a0"
+  url "https://github.com/RiddleMan/doken/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "5f923d9c64400b21471bb02591b59710532ac0b575d979da3cecf6198dddfb0a"
   license any_of: ["MIT"]
   head "https://github.com/RiddleMan/doken.git", branch: "main"
 
@@ -14,6 +14,6 @@ class Doken < Formula
 
   test do
     output = shell_output("#{bin}/doken --version")
-    assert_match "doken 0.1.0", output
+    assert_match "doken 0.2.0", output
   end
 end
