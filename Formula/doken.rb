4068c977c521400e65399102a6ea25e7186c5b29
Artur Ptaszek <artur@ptaszek.dev>
2023-10-31 20:20:40 +0100

    chore: Update doken
    Closes #3.
    
    Signed-off-by: Artur Ptaszek <1132905+RiddleMan@users.noreply.github.com>

--- Formula/doken.rb
+++ Formula/doken.rb
@@ -1,8 +1,8 @@
 class Doken < Formula
   desc "Tool for getting tokens from OAuth 2/OpenID Connect providers"
   homepage "https://github.com/RiddleMan/doken"
-  url "https://github.com/RiddleMan/doken/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "17f17a524d8678b71582fac7e44cb706cf38e2e323b05940e13d70f3aa904df1"
+  url "https://github.com/RiddleMan/doken/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "e8b3c30393c8e392fb87f7bbb8c8e564acc4287620214de3889df88b3824d2ad"
   license any_of: ["MIT"]
   head "https://github.com/RiddleMan/doken.git", branch: "main"
 
@@ -21,6 +21,6 @@ class Doken < Formula
 
   test do
     output = shell_output("#{bin}/doken --version")
-    assert_match "doken 0.4.0", output
+    assert_match "doken 0.4.1", output
   end
 end
