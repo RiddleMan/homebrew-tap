94d129083a971cf1e19e25b91ae61712229836fe
Artur Ptaszek <1132905+RiddleMan@users.noreply.github.com>
2023-10-31 20:44:15 +0000

    git-branch-history: add 0.1.0 bottle.

--- Formula/git-branch-history.rb
+++ Formula/git-branch-history.rb
@@ -6,6 +6,12 @@ class GitBranchHistory < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/RiddleMan/git-branch-history.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/RiddleMan/homebrew-tap/releases/download/git-branch-history-0.1.0"
+    sha256 cellar: :any,                 ventura:      "d56c378304e72074f083209f99208e897ed311be89a217629562c8caef72f21e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d2f07876409afd60a4536a4450d264d71b775fdb464d733661648f07bc0a2371"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
   depends_on "zlib"
