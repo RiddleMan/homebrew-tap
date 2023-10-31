dbb887d3b6022e7cf12391bfb5d216e0d56a83a1
Artur Ptaszek <1132905+RiddleMan@users.noreply.github.com>
2023-10-31 16:32:49 +0000

    doken: add 0.4.0 bottle.

--- Formula/doken.rb
+++ Formula/doken.rb
@@ -6,6 +6,12 @@ class Doken < Formula
   license any_of: ["MIT"]
   head "https://github.com/RiddleMan/doken.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/RiddleMan/homebrew-tap/releases/download/doken-0.4.0"
+    sha256 cellar: :any_skip_relocation, ventura:      "e4cb32dd6fddd63bf2f7863fb9ff3406962f626977f1e3ab378f1333dadc4bcf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b98db9300600e8738caf4a1c4b5da0b033d09ed55b430d9f44797cb74e92e19c"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
